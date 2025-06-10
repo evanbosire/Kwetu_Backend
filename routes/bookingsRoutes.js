cf4b51f6666cd01c717c6ca13b66c5c7d361fba0
Brian Bosire Evans <evanbosire422@gmail.com>
2025-06-10 18:13:11 +0300

    recovered lost files

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -184,7 +184,28 @@ router.get('/supervisor/tasks', async (req, res) => {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
-// Supervisor assigns coach endpoint
+
+
+// // Supervisor assigns coach
+// router.patch('/:id/assign-coach', async (req, res) => {
+//   try {
+//     const booking = await Booking.findByIdAndUpdate(
+//       req.params.id,
+//       { assignedCoach: true },  
+//       { new: true }
+//     );
+    
+//     if (!booking) {
+//       return res.status(404).json({ message: 'Booking not found' });
+//     }
+
+//     res.json(booking);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Server error', error: error.message });
+//   }
+// });
+
+// Updated Supervisor assigns coach endpoint
 router.patch('/:id/assign-coach', async (req, res) => {
   try {
     const { coachId, coachName } = req.body; // Get coach details from request body
@@ -274,7 +295,7 @@ router.patch('/:id/confirm-service', async (req, res) => {
   );
   res.json(booking);
 });
-// Service Manager views all tasks confirmed by supervisor
+// Service Manager views all tasks confirmed by them
 router.get('/supervisor/confirmed-tasks', async (req, res) => {
   try {
     const confirmedTasks = await Booking.find({ supervisorConfirmed: true })
